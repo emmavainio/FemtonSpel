1039f90d989eefe6d6efa03b7c816e0bd3457186
Maria Hagelin <mthagelin@gmail.com>
2022-10-31 13:48:44 +0100

    SwitchPlaces method broken out of main

--- src/Kladd.java
+++ src/Kladd.java
@@ -48,29 +48,11 @@ public class Kladd extends JFrame implements MouseListener, ActionListener {
     public void mouseClicked(MouseEvent e) {
         int indexBlank = 0;
         int indexSiffra;
-
         if (e.getSource() instanceof JLabel) {
             indexSiffra = labelList.indexOf((JLabel) e.getSource());
-
-            for (JLabel jl : labelList) {
-                if (jl.getText().equals(" ")) {
-                    indexBlank = labelList.indexOf(jl);
-                }
-            }
-
-            if (indexBlank % 4 == indexSiffra % 4 && Math.abs((indexBlank / 4) - (indexSiffra / 4)) == 1
-            || Math.abs(indexBlank - indexSiffra) == 1 && !((indexSiffra % 4 == 0 && indexBlank % 4 == 3) ||
-                    (indexSiffra % 4 == 3 && indexBlank % 4 == 0))) {
-                spelGrid.removeAll();
-                Collections.swap(labelList, indexSiffra, indexBlank);
-                for (JLabel jLabel : labelList) {
-                    spelGrid.add(jLabel);
-                }
-                if (labelList.equals(correctList))
-                    winnerMess.setText("Du vann!");
-                revalidate();
-                repaint();
-            }
+            SwitchPlaces sp = new SwitchPlaces(spelGrid, labelList, correctList, indexBlank, indexSiffra, winnerMess);
+            revalidate();
+            repaint();
         }
     }
     @Override
---
+++ src/SwitchPlaces.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SwitchPlaces {
+    private JPanel spelGrid = new JPanel();
+    private List<JLabel> labelList;
+    private List<JLabel> correctList;
+    private int indexBlank;
+    private int indexSiffra;
+    private JLabel winnerMess;
+
+    SwitchPlaces(JPanel spelGrid, List<JLabel> labelList, List<JLabel> correctList, int indexBlank, int indexSiffra, JLabel winnerMess){
+        this.spelGrid=spelGrid;
+        this.labelList=labelList;
+        this.indexBlank=indexBlank;
+        this.indexSiffra=indexSiffra;
+
+        for (JLabel jl : labelList) {
+            if (jl.getText().equals(" ")) {
+                indexBlank = labelList.indexOf(jl);
+            }
+        }
+
+        if (indexBlank % 4 == indexSiffra % 4 && Math.abs((indexBlank / 4) - (indexSiffra / 4)) == 1
+                || Math.abs(indexBlank - indexSiffra) == 1 && !((indexSiffra % 4 == 0 && indexBlank % 4 == 3) ||
+                (indexSiffra % 4 == 3 && indexBlank % 4 == 0))) {
+            spelGrid.removeAll();
+            Collections.swap(labelList, indexSiffra, indexBlank);
+            for (JLabel jLabel : labelList) {
+                spelGrid.add(jLabel);
+            }
+            if (labelList.equals(correctList))
+                winnerMess.setText("Du vann!");
+     //       revalidate();
+       //     repaint();
+        }
+    }
+}
