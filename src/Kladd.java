42ff311b0d852534bd0010ef5c96042cac8596b0
Emma Vainio <emmavainio@live.se>
2022-11-01 15:13:19 +0100

    Changed mouselistener to adapter

--- src/Kladd.java
+++ src/Kladd.java
@@ -12,27 +12,27 @@ import static javax.swing.SwingConstants.CENTER;
 public class Kladd extends JFrame implements ActionListener {
     List<JLabel> labelList = new ArrayList<>();
     List<JLabel> correctList = new ArrayList<>();
-    JPanel spelGrid = new JPanel();
-    JButton nyttSpelKnapp = new JButton("««★\uD83C\uDF1FNytt spel!\uD83C\uDF1F★✴»»");
+    JPanel gamePanel = new JPanel();
+    JButton newGameButton = new JButton("««★\uD83C\uDF1FNytt spel!\uD83C\uDF1F★✴»»");
     JPanel panel = new JPanel();
     JLabel winnerMess = new JLabel(" ");
-    ImageIcon backgroundImage;
-    JLabel labelBackgroundImage;
+    //ImageIcon backgroundImage;
+    JLabel labelBackgroundImage = new JLabel(new ImageIcon("src/pinkDroplet.jpg"));
     Border border = BorderFactory.createLineBorder(new Color(139, 84, 154, 147), 1);
 
     public Kladd() {
         setTitle("Femtonspelet á rosa");
 
-        nyttSpelKnapp.setBackground(new Color(0x794086));
-        nyttSpelKnapp.setForeground(new Color(0xCBCBE0));
-        nyttSpelKnapp.setPreferredSize(new Dimension(330,40));
-        nyttSpelKnapp.setFont(new Font("Monospaced", Font.BOLD, 18));
-        nyttSpelKnapp.setFocusPainted(false);
+        newGameButton.setBackground(new Color(0x794086));
+        newGameButton.setForeground(new Color(0xCBCBE0));
+        newGameButton.setPreferredSize(new Dimension(330,40));
+        newGameButton.setFont(new Font("Monospaced", Font.BOLD, 18));
+        newGameButton.setFocusPainted(false);
 
-        backgroundImage = new ImageIcon(this.getClass().getResource("pinkDroplet.jpg"));
-        labelBackgroundImage = new JLabel(backgroundImage);
+        //backgroundImage = new ImageIcon("src/pinkDroplet.jpg");
+        //labelBackgroundImage = new JLabel(backgroundImage);
 
-        spelGrid.add(labelBackgroundImage);
+        gamePanel.add(labelBackgroundImage);
         labelBackgroundImage.setLayout(new GridLayout(4,4));
         labelBackgroundImage.setPreferredSize(new Dimension(330,330));
 
@@ -40,7 +40,7 @@ public class Kladd extends JFrame implements ActionListener {
         for (int i = 0; i < 16; i++) {
             labelList.add(new JLabel(String.valueOf(i+1)));
             labelBackgroundImage.add(labelList.get(i));
-            labelList.get(i).addMouseListener(this);
+            labelList.get(i).addMouseListener(mouseClickListener);
             correctList.add(labelList.get(i));
 
             labelList.get(i).setForeground(new Color(136, 61, 132, 163));
@@ -52,17 +52,17 @@ public class Kladd extends JFrame implements ActionListener {
         labelList.get(labelList.size()-1).setText(" ");
 
         panel.setLayout(new BorderLayout());
-        panel.add(nyttSpelKnapp, BorderLayout.NORTH);
-        panel.add(spelGrid, BorderLayout.CENTER);
+        panel.add(newGameButton, BorderLayout.NORTH);
+        panel.add(gamePanel, BorderLayout.CENTER);
         panel.setBackground(new Color(0x794086));
 
         panel.add(winnerMess, BorderLayout.SOUTH);
         winnerMess.setFont(new Font("Monospaced", Font.BOLD, 18));
         winnerMess.setForeground(new Color(0xFFFFFF));
-      //  winnerMess.setBackground(new Color(0x402757));
+        //winnerMess.setBackground(new Color(0x402757));
         winnerMess.setHorizontalAlignment(CENTER);
         winnerMess.setPreferredSize(new Dimension(330, 40));
-        nyttSpelKnapp.addActionListener(this);
+        newGameButton.addActionListener(this);
 
         add(panel);
         panel.setPreferredSize(new Dimension(400, 420));
@@ -79,15 +79,20 @@ public class Kladd extends JFrame implements ActionListener {
             int indexSiffra;
             if (e.getSource() instanceof JLabel) {
                 indexSiffra = labelList.indexOf((JLabel) e.getSource());
-                SwitchPlaces sp = new SwitchPlaces(spelGrid, labelList, correctList, indexBlank, indexSiffra, winnerMess);
+                SwitchPlaces sp = new SwitchPlaces(gamePanel, labelList, correctList, indexBlank, indexSiffra, winnerMess);
                 revalidate();
                 repaint();
             }
+            if (labelList.contains(e.getSource())) {
+                indexSiffra = labelList.indexOf((JLabel) e.getSource());
+                SwitchPlaces sp = new SwitchPlaces(gamePanel, labelList, correctList, indexBlank, indexSiffra, winnerMess);
+                revalidate();
+                repaint();
         }
     };
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == nyttSpelKnapp) {
+        if (e.getSource() == newGameButton) {
             labelBackgroundImage.removeAll();
             Collections.shuffle(labelList);
             for (JLabel jLabel : labelList) {
